1ac857beda69ab444e6a630fc642026d29a21fef
Layton Vincent-Stewart <10005924@student.toiohomai.ac.nz>
2018-07-26 14:48:39 +1200

    Hello World Second Commit

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -18,6 +18,47 @@ namespace HelloWorld
 
             //Use console readline to stop automatically closing
             Console.ReadLine();
+
+            string aTutor = "Murray in 303";
+            string bTutor = "Jacob in 308";
+            string cTutor = "Stefan in 219";
+            string dTutor = "Ray";
+            Console.WriteLine($"Our tutors are {aTutor}, {bTutor}, {cTutor} and {dTutor}");
+
+            Console.ReadLine();
+
+            Console.WriteLine("Our current class is");
+            string input1 = Console.ReadLine();
+
+            Console.WriteLine($"Our current class is {input1}");
+            Console.WriteLine("Our current class is {0}",input1);
+            Console.WriteLine($"Our current class is " + input1);
+
+            if(input1 == "5202")
+            {
+                Console.WriteLine("your teacher is " + bTutor);
+            }
+
+            if (input1 == "Wed 1" || input1 == "Tues 1")
+            {
+                Console.WriteLine("your teacher is " + cTutor);
+            }
+
+            if (input1 == "Prog")
+            {
+                Console.WriteLine("Programming Classes are 3+4 Mon, 4 Wed, and 1+2 Thursday with " + bTutor);
+            }
+
+            if (input1 == "Enterprise")
+            {
+                Console.WriteLine("Enterprise Classes are 1+2 Mon, 3 Wed, and 3+4 Thursday with " + aTutor);
+            }
+
+
+            Console.ReadLine();
+
+
+
         }
     }
 }
