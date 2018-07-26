56a67fbab51697c0c15809549e5b95f65eeb0585
Layton Vincent-Stewart <10005924@student.toiohomai.ac.nz>
2018-07-26 13:08:45 +1200

    Hello World First Commit

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string output = "Hello World";
+            Console.WriteLine(output);
+
+            //Or it can be done as a single line
+            Console.WriteLine("Hello World 1");
+
+            //Use console readline to stop automatically closing
+            Console.ReadLine();
+        }
+    }
+}
